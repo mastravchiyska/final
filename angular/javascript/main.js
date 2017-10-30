91a3b286f37e3c0333f8cf8025d81739f6dedc90
melinil <doncho.dobrev@gmail.com>
2017-10-30 12:54:11 +0200

    Static frontend structure

--- angular/javascript/main.js
+++
@@ -1,6 +0,0 @@
-var app = angular.module('myApp', []);
-app.controller('userController', function($scope, $http) {
-    $http.get('user').then(function(response) {
-        $scope.user = response.data;
-    });
-});
