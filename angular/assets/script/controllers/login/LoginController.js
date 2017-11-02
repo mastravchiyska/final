ceaa6406297925236d350ad27c757c0ac8725e90
melinil <doncho.dobrev@gmail.com>
2017-11-02 15:06:49 +0200

    added sessions

--- angular/assets/script/controllers/login/LoginController.js
+++ angular/assets/script/controllers/login/LoginController.js
@@ -1,9 +1,14 @@
 app.controller('LoginController', ['$scope', 'LoginService', function ($scope, LoginService) {
     $scope.user = '';
-    
+    document.getElementById('header1').style.display = "none";
     $scope.login = function() {
+        var email = document.getElementById("loginemail").value;
+        var pass = document.getElementById("loginpass").value;
         LoginService.login({ "email": "Doncho@mail.com", "password": "verySecure1!" }).then(function(result) {
-            $scope.user = result.data.name;
+            console.log(result.data.data.name);
+            sessionStorage.setItem('session_id', result.data.data._id);
+            location.reload();
+            
         });
     };
 
--- angular/main.js
+++ angular/main.js
@@ -1,20 +1,22 @@
-var app = angular.module('tdApp', ['ngRoute']).
-config(['$routeProvider', function($routeProvider) {
-    $routeProvider
-        .when('/', {
-            templateUrl: 'assets/script/controllers/login/login.htm',
-            controller: 'LoginController'
-        });
-        /*.when('/accountDetails', {
-            templateUrl: 'assets/script/views/accountDetails.htm'
-        })
-        .when('/accountSetting', {
-            templateUrl: 'assets/script/views/accountSetting.htm'
-        })
-        .when('/pictureList', {
-            templateUrl: 'assets/script/views/pictureList.htm'
-        })
-        .when('/searchScreen', {
-            templateUrl: 'assets/script/views/searchScreen.htm'
-        });*/
-}]);
+if (sessionStorage.length == 0) {
+    var app = angular.module('tdApp', ['ngRoute']).
+        config(['$routeProvider', function ($routeProvider) {
+            $routeProvider
+                .when('/', {
+                    templateUrl: 'assets/script/controllers/login/login.htm',
+                    controller: 'LoginController'
+                });
+        }]);
+    
+} else {
+    var app = angular.module('tdApp', ['ngRoute']).
+        config(['$routeProvider', function ($routeProvider) {
+            $routeProvider
+                .when('/', {
+                    templateUrl: 'assets/script/controllers/main/main.htm',
+                    controller: 'mainController'
+                });
+        }]);
+}
+
+
