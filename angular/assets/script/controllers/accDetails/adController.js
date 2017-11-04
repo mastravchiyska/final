3c748c8aefe0d6f4d2f8a9a4ea93d6f2f7af5f93
melinil <doncho.dobrev@gmail.com>
2017-11-04 14:47:05 +0200

    small patch up

--- angular/assets/script/controllers/accDetails/adController.js
+++ angular/assets/script/controllers/accDetails/adController.js
@@ -1,12 +1,10 @@
 app.controller('adController', ['$scope', 'adService', function ($scope, adService) {
+    var sessionId = sessionStorage.getItem('session_id')
 
     $scope.showDetails = function () {
-        var sessionId = sessionStorage.getItem('session_id')
-
-        LoginService.showDetails({ "_id": sessionId }).then(function (result) {
-
-            // $scope.fname = result.data.data.name;
+        adService.showDetails({ "_id": sessionId }).then(function (result) {
             console.log(result.data.data.name);
         });
     };
 }]);
+
