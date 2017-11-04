02cf0ad626d5a074a538ea00c52c343ab1a1cf13
mastravchiyska <tatyana.mastravchiyska@gmail.com>
2017-11-04 14:16:08 +0200

    Test user profile

--- angular/assets/script/controllers/accDetails/adController.js
+++ angular/assets/script/controllers/accDetails/adController.js
@@ -3,10 +3,10 @@ app.controller('adController', ['$scope', 'adService', function ($scope, adServi
     $scope.showDetails = function () {
         var sessionId = sessionStorage.getItem('session_id')
 
-        LoginService.showDetails({ "_id": sessionId }).then(function (result) {
+        adService.showDetails({ "_id": sessionId }).then(function (result) {
 
             // $scope.fname = result.data.data.name;
-            console.log(result.data.data.name);
+            console.log(result);
         });
     };
 }]);
--- angular/assets/script/controllers/accDetails/adService.js
+++ angular/assets/script/controllers/accDetails/adService.js
@@ -2,7 +2,7 @@ app.factory('adService', function ($http, $rootScope) {
     
     function getInfo() { }
     
-    getInfo.prototype.getAccInfo = function (data) {
+    getInfo.prototype.showDetails = function (data) {
         return new Promise(function(resolve, reject) {
             $http.get('http://localhost:4000/user/profile', data).then(function(response) {
                 resolve(response);
