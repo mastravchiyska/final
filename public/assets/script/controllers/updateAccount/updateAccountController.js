a44d568e6cdd86184210bd48d182cb18c7ebd78f
melinil <doncho.dobrev@gmail.com>
2017-11-05 17:56:03 +0200

    added updated account function

---
+++ public/assets/script/controllers/updateAccount/updateAccountController.js
@@ -0,0 +1,23 @@
+app.controller('updateAccountController', ['$scope', 'updateAccountService',
+    function ($scope, updateAccountService) {
+        $scope.updateForm = {
+            id: '',
+            name: '',
+            lastname: '',
+            email: '',
+            password: '',
+            birthday: '',
+            sex: ''
+        };
+
+
+        $scope.update = function () {
+            var user = JSON.parse(localStorage.getItem("user"))
+            var data = $scope.updateForm;
+            data.id = user._id;
+            data.sex = user.sex;
+            updateAccountService.updateInfo(data).then(function (result) {
+                console.log(result.d);
+            })
+        };
+    }]);
---
+++ public/assets/script/controllers/updateAccount/updateAccountService.js
@@ -0,0 +1,14 @@
+app.factory('updateAccountService', function ($http, $rootScope) {
+    
+    function Update() { }
+    
+    Update.prototype.updateInfo= function (data) {
+        return new Promise(function(resolve, reject) {
+            $http.post('http://localhost:4000/user/update', data).then(function(response) {
+                resolve(response);
+            });
+        });
+    }
+    
+    return new Update();
+});
