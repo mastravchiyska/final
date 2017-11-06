50d985b03b146f893580e8f5dfdbf1a6205fcfab
melinil <doncho.dobrev@gmail.com>
2017-11-06 22:08:42 +0200

    a minor patchup

--- public/assets/script/controllers/friendRequests/friendRequestsService.js
+++ public/assets/script/controllers/friendRequests/friendRequestsService.js
@@ -1,33 +1,22 @@
 app.factory('friendRequestsService', function ($http, $rootScope) {
-    
+
     function ListRequests() { }
-    
-    ListRequests.prototype.listRequests= function (data) {
-        
-        return new Promise(function(resolve, reject) {
-            $http.get('http://localhost:4000/friend/requestList', data).then(function(response) {
+    ListRequests.prototype.listRequests = function (data) {
+        return new Promise(function (resolve, reject) {
+            $http.get('http://localhost:4000/friend/requestList', data).then(function (response) {
                 resolve(response);
             });
         });
     }
 
-    // ListRequests.prototype.listRequests= function (data) {
-        
-    //     return new Promise(function(resolve, reject) {
-    //         $http.get('http://localhost:4000/friend/requestList', data).then(function(response) {
-    //             resolve(response);
-    //         });
-    //     });
-    // }
 
-    // ListRequests.prototype.listRequests= function (data) {
-        
-    //     return new Promise(function(resolve, reject) {
-    //         $http.get('http://localhost:4000/friend/removeRequest/'+id, data).then(function(response) {
-    //             resolve(response);
-    //         });
-    //     });
-    // }
-    
+    ListRequests.prototype.acceptRequest = function (data) {
+
+        return new Promise(function (resolve, reject) {
+            $http.get('http://localhost:4000/friend/add/'+id, data).then(function (response) {
+                resolve(response);
+            });
+        });
+    }
     return new ListRequests();
 });
--- server/routes/friend.js
+++ server/routes/friend.js
@@ -14,9 +14,9 @@ function checkForSession(req) {
     return sessionId;
 }
 
-router.get('/add/:id', function (req, res) {
+router.get('/add/:friendId', function (req, res) {
     var id = checkForSession(req);
-    var friendId = req.params.id;
+    var friendId = req.params.friendId;
     if (id) {
         friendModel.addFriend(id, friendId).then(function (data) {
             res.json({ message: 'The friend was added!' });
